ed0d9cbb3b29c979eb838d1f3a210c402bbfa828
Darupi <darupi@msn.com>
2011-09-03 10:25:43 +0700

    First Commit
    Window Based Template

---
+++ DummyPrj/DummyPrjAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DummyPrjAppDelegate.h
+//  DummyPrj
+//
+//  Created by JOHN MOLLOY on 3/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DummyPrjAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
